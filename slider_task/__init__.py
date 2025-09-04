d9e25e0ba72a386729fd86dc452ea4ad3130eedf
chkgk <koenig.kersting@gmail.com>
2025-09-04 13:48:21 +0200

    updated slider task

--- slider_task/__init__.py
+++ slider_task/__init__.py
@@ -1,5 +1,5 @@
 from otree.api import *
-
+import random
 
 doc = """
 Your app description
@@ -21,20 +21,26 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    pos_1 = models.IntegerField(min=0, max=100)
+    pos_2 = models.IntegerField(min=0, max=100)
+    pos_3 = models.IntegerField(min=0, max=100)
+    pos_4 = models.IntegerField(min=0, max=100)
+    pos_5 = models.IntegerField(min=0, max=100)
+    
+    sliders_in_middle = models.IntegerField()
 
+# FUNCTIONS
+def creating_session(subsession: Subsession):
+    for player in subsession.get_players():
+        for i in range(1, 6):
+            setattr(player, f"pos_{i}", random.choice((random.randint(5, 46), random.randint(55, 96))))
 
 # PAGES
-class MyPage(Page):
-    pass
+class RealEffort(Page):
+    form_model = 'player'
+    form_fields = ['pos_1', 'pos_2', 'pos_3', 'pos_4', 'pos_5']
 
+    def before_next_page(player, timeout_happened):
+        player.sliders_in_middle = sum(1 for pos in [player.pos_1, player.pos_2, player.pos_3, player.pos_4, player.pos_5] if pos == 50)
 
-class ResultsWaitPage(WaitPage):
-    pass
-
-
-class Results(Page):
-    pass
-
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [RealEffort]
--- slider_task/tests.py
+++ slider_task/tests.py
@@ -3,4 +3,10 @@ import random
 
 class PlayerBot(Bot):
     def play_round(self):
-        pass
+        yield RealEffort, {
+            'pos_1': random.randint(1, 100),
+            'pos_2': random.randint(1, 100),
+            'pos_3': random.randint(1, 100),
+            'pos_4': random.randint(1, 100),
+            'pos_5': random.randint(1, 100),
+        }
