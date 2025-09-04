29c4d74c76baeb7faafeb318fe7ea3c3c6247402
chkgk <koenig.kersting@gmail.com>
2025-09-04 10:30:15 +0200

    risk / holt done

--- risk/__init__.py
+++ risk/__init__.py
@@ -1,5 +1,5 @@
 from otree.api import *
-
+import random
 
 doc = """
 Your app description
@@ -11,6 +11,20 @@ class C(BaseConstants):
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
 
+    HL_LOTTERY = {
+        "name": "HL1",
+        "lottery_a": {
+            "probabilities": [0.5, 0.5],
+            "prizes": [
+                [0.2 for _ in range(0, 10)],
+                [4.2 for _ in range(0, 10)]
+            ]
+        },
+        "lottery_b": {
+            "probabilities": [1],
+            "prizes": [[0.6, 1.0, 1.4, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8, 4.2]]
+        },
+    }
 
 class Subsession(BaseSubsession):
     pass
@@ -21,20 +35,48 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    hl_a_1 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_2 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_3 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_4 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_5 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_6 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_7 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_8 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_9 = models.BooleanField(widget=widgets.RadioSelect)
+    hl_a_10 = models.BooleanField(widget=widgets.RadioSelect)
+
+# FUNCTIONS
+def calculate_payoff(player: Player, lottery):
+    max_row = len(lottery["lottery_a"]["prizes"][0])
+    random_row = random.randint(1, max_row)
+    chose_a = getattr(player, f"hl_a_{random_row}")
+
+    pay_left = random.random() < lottery["lottery_a"]["probabilities"][0]
+    if not chose_a:
+        lottery_outcome = lottery["lottery_b"]["prizes"][0][random_row - 1]
+
+    else:
+        if pay_left:
+            lottery_outcome = lottery["lottery_a"]["prizes"][0][random_row - 1]
+        else:
+            lottery_outcome = lottery["lottery_a"]["prizes"][1][random_row - 1]
+
+    player.payoff = lottery_outcome
 
 
 # PAGES
-class MyPage(Page):
-    pass
+class HL(Page):
+    form_model = 'player'
+    form_fields = [f"hl_a_{i}" for i in range(1, 11)]
+
+    def js_vars(player: Player):
+        return {
+            'lotteries': C.HL_LOTTERY,
+        }
+
+    def before_next_page(player: Player, timeout_happened):
+        calculate_payoff(player, C.HL_LOTTERY)
 
 
-class ResultsWaitPage(WaitPage):
-    pass
-
-
-class Results(Page):
-    pass
-
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [HL]
