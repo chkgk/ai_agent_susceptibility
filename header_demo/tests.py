e08f7c70794fd00919ac4d9bc88b886f26a4b0b4
chkgk <koenig.kersting@gmail.com>
2025-09-04 09:04:11 +0200

    header tests starting point

---
+++ header_demo/tests.py
@@ -0,0 +1,9 @@
+from otree.api import Currency as c, currency_range, expect, Bot
+from . import *
+import random
+
+class PlayerBot(Bot):
+    def play_round(self):
+        yield MyPage
+        yield Results
+        yield Results2
